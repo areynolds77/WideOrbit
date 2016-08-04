a0b45495899604dd9130b8b845c8c8ece99b49ad
Alexander Reynolds <areynolds@etmcorad.com>
2016-08-04 14:38:21 -0400

    Added Module Functionality & Install Script

---
+++ Install-WideOrbitPSModule.ps1
@@ -0,0 +1,35 @@
+param([string]$InstallDirectory)
+
+$filelist = echo `
+    Import-WideOrbit.psd1 `
+    Import-WideOrbit.psm1 `
+    Export-MediaAssets.ps1 `
+    Get-AllRadioStations.ps1 `
+    Get-MediaAsset.ps1 `
+    Get-ProblemCarts.ps1 `
+    Get-ScheduleByDate.ps1 `
+    Import-MediaAsset.ps1
+
+if ('' -eq $InstallDirectory) {
+    $personalModules = Join-Path -Path ([System.Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowershell\Modules
+
+    if(($env:PSModulePath -split ';') -notcontains $personalModules) {
+        Write-Warning "personalModules is not in `$env:PSModulePath"
+    }
+
+    if(!(Test-Path $personalModules)) {
+        Write-Error "$personalModules does not exist."
+    }
+
+    $InstallDirectory = Join-Path -Path $personalModules -ChildPath WideOrbit
+}
+
+if (!(Test-Path $InstallDirectory)) {
+    $null = mkdir $InstallDirectory
+}
+
+$wc = New-Object System.Net.WebClient
+$filelist | ForEach-Object {
+    $wc.DownloadFile("https://raw.github.com/areynolds77/wideorbit/master/$_","$InstallDirectory\$_")
+}
+    
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # WideOrbit
 WideOrbit Powershell Module
+##Installation
+Powershell V4 and Earlier
+To install to your personal modules folder (e.g. ~\Documents\WindowsPowerShell\Modules), run:
+```powershell
+iex (new-object System.Net.WebClient).DownloadString('https://raw.github.com/areynolds77/wideorbit/master/InstallWideOrbitPSModule.ps1')
+```
+
+OR 
+Download the 'InstallWideOrbitPSModule.ps1' file and execute it.
+
 ## Functions
+
 #### Get-MediaAsset
 #### Get-ProblemCarts
 #### Get-ScheduleByDate
