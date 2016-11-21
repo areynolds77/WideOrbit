99c3ae62fe1d9f48a5523f165d1ccdac08f97b9b
Alexander Reynolds <areynolds@etmcorad.com>
2016-11-21 10:39:32 -0500

    Updated manfiest

--- WideOrbit.psm1
+++ WideOrbit.psm1
@@ -1,8 +1,8 @@
 . $PSScriptRoot\Export-MediaAssets.ps1
 . $PSScriptRoot\Get-AllRadioStations.ps1
 . $PSScriptRoot\Get-MediaAsset.ps1
-. $PSScriptRoot\Get-ProblemCarts.ps1
 . $PSScriptRoot\Get-ScheduleByDate.ps1
 . $PSScriptRoot\Remove-MediaAsset.ps1
+. $PSScriptRoot\Remove-CueAudio.ps1
 . $PSScriptRoot\Search-RadioStationContent.ps1
 . $PSScriptRoot\Sync-WOPurge.ps1
